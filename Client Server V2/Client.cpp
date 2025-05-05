70b8d706145ea64f10bd3ba19a836a1d1bff8449
VS <vaish261@gmail.com>
2025-05-05 09:38:09 +0200

    Add project files.

---
+++ Client Server V2/Client.cpp
@@ -0,0 +1,166 @@
+	#include <iostream>
+	#include <WinSock2.h>
+	#include <Ws2tcpip.h> 
+	#include <conio.h>
+	#include <windows.h>
+	#include <thread>
+	#include <vector>
+	#include <string>
+	#include <mutex>
+	#pragma comment(lib, "Ws2_32.lib")
+	
+	std::vector<std::string>messagesConstant;
+	std::mutex messagesConstantMutex;
+
+	bool clientStart(WSADATA& wsa) {
+
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+
+			std::cout << "Startup failed";
+			return false;
+
+		}
+
+		else {
+
+			return  true;
+
+		}
+
+	}
+
+	SOCKET clientConnection(sockaddr_in &server) {
+
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET) {
+
+			std::cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return 1;
+
+		}
+
+		server.sin_family = AF_INET;
+		server.sin_port = htons(1111);
+
+		const char* ip = "127.0.0.1";//rem to change this to designated ip
+
+		if (inet_pton(AF_INET, ip, &server.sin_addr) <= 0) {
+
+			std::cerr << "Invalid address or address not supported" << std::endl;
+			return 1;
+
+		}
+		return sock;
+	}
+
+	void moveCursorToTop() {
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		COORD coordScreen = { 0, 0 };
+		SetConsoleCursorPosition(hConsole, coordScreen);
+	}
+
+
+	void redrawScreen() {
+		moveCursorToTop();
+
+		std::lock_guard<std::mutex> lock(messagesConstantMutex);
+
+		DWORD written;
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		GetConsoleScreenBufferInfo(hConsole, &csbi);
+		FillConsoleOutputCharacter(hConsole, ' ', csbi.dwSize.X * csbi.dwSize.Y, { 0,0 }, &written);
+		SetConsoleCursorPosition(hConsole, { 0,0 });
+
+		for (const auto& msg : messagesConstant) {
+			std::cout << msg << "\n";
+		}
+	}
+
+	void chat(SOCKET sock) {
+
+		std::string mes, user;
+		char buffer[2048] = {};
+		const char* message;
+		const char* clientName = {};
+
+		std::cout << "Enter Username: ";
+		std::getline(std::cin, user);
+		clientName = user.c_str();
+		if (clientName == NULL) {
+			std::cout << "\nERROR GETTING USERNAME, restart program:\n";
+			return;
+		}
+		send(sock, clientName, static_cast<int>(strlen(clientName)), 0);
+		while (true) {
+
+			std::cout << "Enter Message(type '/exit' to exit,'/help' for help): ";
+			std::getline(std::cin, mes);
+			if (mes == "/exit") {
+				std::cout << "Closing";
+				closesocket(sock);
+				WSACleanup();
+				return;
+			}
+			message = mes.c_str();
+
+			send(sock, message, static_cast<int>(strlen(message)), 0);
+
+			{
+				std::lock_guard<std::mutex>lock(messagesConstantMutex);
+				messagesConstant.push_back(user+": "+mes.c_str());
+			}
+			redrawScreen();
+
+		}
+	}
+
+	void recieveMessages(SOCKET sock) {
+		char buffer[2048] = {};
+		int serverReply;
+		while(true){
+			serverReply = recv(sock, buffer, sizeof(buffer) - 1, 0);
+			if (serverReply > 0) {
+
+				buffer[serverReply] = '\0';
+				{
+					std::lock_guard<std::mutex>lock(messagesConstantMutex);
+					messagesConstant.push_back(std::string(buffer))	;
+				}
+
+			}
+			std::cout << "\n" << buffer << std::endl;
+			memset(buffer, 0, sizeof(buffer));
+			redrawScreen();
+		}
+	}
+
+	void cleanUp(SOCKET sock) {
+		closesocket(sock);
+		WSACleanup();
+	}
+
+
+
+	
+
+	int main() {
+
+		WSADATA wsa;
+		SOCKET sock;
+		sockaddr_in server = {};
+		clientStart(wsa);
+
+		sock = clientConnection(server);
+
+		if (connect(sock, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+			std::cerr << "Connection failed with error: " << WSAGetLastError() << std::endl;
+		}
+		std::thread messageReciever(recieveMessages, sock);
+		chat(sock);
+	
+		cleanUp(sock);
+
+
+	}
---
+++ Client Server V2/Server.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h> 
+#include <conio.h>
+#include <thread>
+#include <vector>
+#include <atomic>
+#include<mutex>
+#include <cctype>
+
+
+#pragma comment(lib, "Ws2_32.lib")
+
+std::atomic<int> count = 0;
+std::vector<std::string>connectedUsers;
+std::vector<SOCKET>clientSockets;
+std::mutex userMutex,socketMutex;
+
+bool serverStart(WSADATA &wsa){
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+
+		std::cout << "Startup failed";
+		return false;
+
+	}
+
+	else {
+
+		return  true;
+
+	}
+
+}
+
+SOCKET connection(sockaddr_in& server) {
+
+	SOCKET serverSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSock == INVALID_SOCKET) {
+
+		std::cout << WSAGetLastError;
+		return INVALID_SOCKET;
+
+	}
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(1111);
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	//const char* ip = INADDR_ANY; //rem to change this to designated ip
+
+	/*if (inet_pton(AF_INET, ip, &server.sin_addr) <= 0) {
+
+		std::cerr << "Invalid address or address not supported" << std::endl;
+		return INVALID_SOCKET;
+
+	}
+	*/
+
+	if (bind(serverSock, (sockaddr*) &server, sizeof(server)) == SOCKET_ERROR) {
+
+		std::cout << "Bind failed" << WSAGetLastError() << "\n";
+		return INVALID_SOCKET;
+
+	}
+
+	if (listen(serverSock, 3) == SOCKET_ERROR) {
+
+		std::cout << "Listener Failed" << WSAGetLastError() << "\n";
+		return INVALID_SOCKET;
+
+	}
+
+	std::cout << "Server Started\n";
+	return serverSock;
+}
+
+void userList(SOCKET clientSock) {
+
+	std::string list = "\nConnected Users \n";
+
+	{
+		std::lock_guard<std::mutex> lock(userMutex);
+		for (const auto& user : connectedUsers) {
+			list += "-" + user + "\n";
+		}
+	}
+	send(clientSock, list.c_str(), list.size(), 0);
+}
+
+void chat(SOCKET clientSock) {
+
+	if (clientSock == INVALID_SOCKET) {
+
+		std::cout << "ERROR CONNECTING" << WSAGetLastError() << "\n";
+
+	}
+
+	else {
+
+		std::cout << "\nConnected to Client" << std::endl;
+
+	}
+
+	char buffer[1024] = {};
+	char usernameBuffer[256] = {};
+	char checkBuffer[256] = {};
+	std::string username, messageBroadcasted;
+	int counter = 30000, byteReceived, inc = 0, usernameReceived, lowerCheck;
+
+	setsockopt(clientSock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&counter, sizeof(counter));
+	
+	usernameReceived = recv(clientSock, usernameBuffer, sizeof(usernameBuffer) - 1, 0);
+	if (usernameReceived > 0 && usernameReceived < sizeof(usernameBuffer)) {
+
+		usernameBuffer[usernameReceived] = '\0';
+		if (username.empty()) {
+			username = usernameBuffer;
+			{
+				std::lock_guard<std::mutex> lock(userMutex);
+				connectedUsers.push_back(username);
+
+			}
+
+		}
+
+
+	}
+	{
+		std::lock_guard<std::mutex>lock(socketMutex);
+		clientSockets.push_back(clientSock);
+	}
+
+	while (true) {
+		memset(buffer, 0, sizeof(buffer));
+		byteReceived = recv(clientSock, buffer, sizeof(buffer) - 1, 0);
+		
+
+		if (byteReceived == SOCKET_ERROR || byteReceived <= 0) {
+
+			std::cout << "\nClient disconnected\n";
+			count--;
+			std::cout << "\nTotal Number of users: " << count<<"\n";
+
+			std::lock_guard<std::mutex>lock(userMutex);
+			connectedUsers.erase(
+				std::remove(connectedUsers.begin(), connectedUsers.end(), username),
+				connectedUsers.end()
+			);
+			break;
+
+		}
+
+		else {
+
+			inc += 1;
+			lowerCheck = tolower(byteReceived);
+			if (byteReceived > 0 ) {
+
+				buffer[byteReceived] = '\0';
+
+			}
+
+			if (strcmp(buffer, "/users") == 0) {
+				userList(clientSock);
+				continue;
+				memset(buffer, 0, sizeof(buffer));
+			}
+			if (strcmp(buffer, "/help") == 0) {
+				messageBroadcasted = "/users - displays current users\n/help - displays help page";
+				send(clientSock, messageBroadcasted.c_str(), messageBroadcasted.size(), 0);
+				continue;
+				memset(buffer, 0, sizeof(buffer));
+			}
+			
+			else{
+			std::cout << "\nClient: " << username;
+			std::cout << "\nResponse: " << buffer << "\n";
+			messageBroadcasted = username + ": " + buffer;
+			{
+				std::lock_guard <std::mutex> lock (socketMutex);
+				for (SOCKET sock : clientSockets) {
+					if (sock != clientSock) {
+						send(sock, messageBroadcasted.c_str(), messageBroadcasted.size(), 0);
+						std::cout << "\n";
+					}
+				}
+			}
+			}
+		}
+	}
+
+
+}
+
+void Cleanup(SOCKET serverSock, SOCKET clientSock) {
+	closesocket(serverSock);
+	closesocket(clientSock);
+	WSACleanup();
+}
+
+void handleClients(SOCKET clientSock) {
+
+	chat(clientSock);
+	closesocket(clientSock);
+	std::lock_guard<std::mutex> lock(socketMutex);
+	clientSockets.push_back(clientSock);
+}
+
+int main() {
+
+	WSADATA wsa;
+	SOCKET serverSock,clientSock;
+	sockaddr_in server = {};
+	sockaddr_in client = {};
+	int clientSize = sizeof(client);
+	BOOL opt = TRUE;
+	int c;
+	std::vector<std::thread>clientThread;
+
+	serverStart(wsa);
+
+	c = sizeof(sockaddr_in);
+	serverSock = connection(server);
+
+	while(true){
+	clientSock = accept(serverSock, (sockaddr*)&client, &c);
+	if (clientSock == INVALID_SOCKET) {
+		std::cerr << "Accept failed: " << WSAGetLastError() << std::endl;
+	}
+	else {
+		std::cout << "\nClient connected!\n" << std::endl;
+	}
+	clientThread.emplace_back(std::thread(handleClients, clientSock));
+	count++;
+	std::cout << "\nTotal Number of users: "<<count;
+	clientThread.back().detach();
+	}
+	Cleanup(serverSock, clientSock);
+
+}
